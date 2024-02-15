8fe5a00f3004739643b6dcc7404ba31458d31b40
FMaslina <FMaslina@yandex.ru>
2024-02-15 22:29:54 +0700

    Add GetAuthorAndBooksTestCase

--- writers_and_books/tests.py
+++ writers_and_books/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from writers_and_books.models import WriterModel, BookModel
 
-# Create your tests here.
+
+class GetAuthorAndBooksTestCase(APITestCase):
+    def test_get(self):
+        writer = WriterModel.objects.create(name='test')
+        BookModel.objects.create(title='test', author=writer)
+        url = reverse('get_author_and_books_view', args=[1])
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        data = {
+            "id": 1,
+            "name": "test",
+            "books": [
+                {
+                    "id": 1,
+                    "title": "test"
+                }
+            ]
+        }
+        
+        self.assertEqual(data, response.data)
