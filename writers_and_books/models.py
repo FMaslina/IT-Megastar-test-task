ef582ea9ce6848d335816bd9b3272136a8bdb48e
FMaslina <FMaslina@yandex.ru>
2024-02-15 20:36:30 +0700

    Add WriterModel and BookModel

--- writers_and_books/models.py
+++ writers_and_books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class WriterModel(models.Model):
+    name = models.TextField(verbose_name="Имя")
+
+    class Meta:
+        verbose_name = "Писатель"
+        verbose_name_plural = "Писатели"
+
+
+class BookModel(models.Model):
+    author = models.ForeignKey(WriterModel, on_delete=models.CASCADE, verbose_name="Автор")
+    title = models.TextField(verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
