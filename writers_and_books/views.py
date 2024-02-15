7617fa1cde44454228223a2bc597e5dc6cfec257
FMaslina <FMaslina@yandex.ru>
2024-02-15 22:06:38 +0700

    Add GetAuthorAndBooksView

--- writers_and_books/views.py
+++ writers_and_books/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from writers_and_books.misc import get_writer_and_books
 
-# Create your views here.
+
+class GetAuthorAndBooksView(APIView):
+    def get(self, request, *args, **kwargs):
+        pk = kwargs['pk']
+
+        try:
+            data = get_writer_and_books(pk)
+            return Response(data=data, status=status.HTTP_200_OK)
+        except BaseException:
+            return Response(status=status.HTTP_404_NOT_FOUND)
